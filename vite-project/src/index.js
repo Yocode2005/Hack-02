7d708196bad0b5ff4f03d4c30f360d45898f8406
Yogesh kumar meena <yogeshchhandhwal2005@gmail.com>
2026-05-15 22:55:04 +0530

    Fix import statement for Provider from react-redux

--- vite-project/src/index.js
+++ vite-project/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'; // import react
 import ReactDOM from 'react-dom/client'; // import react dom
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'; // import the redux
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
