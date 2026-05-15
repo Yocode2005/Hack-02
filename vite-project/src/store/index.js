fad25b1a9314981b2b38c747cc3c874938a482a5
Yogesh kumar meena <yogeshchhandhwal2005@gmail.com>
2026-05-15 22:53:32 +0530

    add location

--- vite-project/src/store/index.js
+++ vite-project/src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'; // import redux toolkit
 import authReducer from './slices/authSlice'; // import slices from auth
 import alertReducer from './slices/alertSlice'; // import slices from alertslice
-import locationReducer from './slices/locationSlice';
+import locationReducer from './slices/locationSlice'; // import slice from locationSlice
 export const store = configureStore({
  reducer: {
  auth: authReducer,
